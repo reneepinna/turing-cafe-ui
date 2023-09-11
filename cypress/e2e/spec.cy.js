277eb480cf85876f1fffae43536515df3e67efd0
Renee Pinna <renee.pinna@gmail.com>
2023-09-11 11:19:39 -0600

    Add form testing and fixture

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,26 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('reservations', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+      fixture: 'resData.json',
+    });
+  });
+
+  it('should  be able to add a reservation', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('.reservationCard').first().should('contain', 'Christie');
+    cy.get('.reservationCard').last().should('contain', 'Pam');
+
+    cy.get('#name').type('Renee')
+    cy.get('#date').type('03/08')
+    cy.get('#time').type('12:30')
+    cy.get('#number').type('2')
+    cy.get('input[type=submit]').click()
+
+    cy.get('.reservationCard').last().should('contain', 'Renee');
+    cy.get('.reservationCard').last().should('contain', '03/08');
+    cy.get('.reservationCard').last().should('contain', '12:30 pm');
+    cy.get('.reservationCard').last().should('contain', 'Number of guests: 2');
+
+  });
+});
