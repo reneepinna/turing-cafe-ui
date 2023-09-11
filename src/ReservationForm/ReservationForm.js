a043a00ad7215fff912a50fb154bea9140a22be6
Renee Pinna <renee.pinna@gmail.com>
2023-09-11 11:58:21 -0600

    Add minor error handling to form

--- src/ReservationForm/ReservationForm.js
+++ src/ReservationForm/ReservationForm.js
@@ -9,6 +9,16 @@ function ReservationForm({ setReservations }) {
     time: '',
     number: '',
   });
+  const [formError, setFormError] = useState('');
+
+  function validate({ name, date, time, number }) {
+    if (![name, date, time, number].every(input => input)) {
+      setFormError('All inputs are required.')
+      return false;
+    }
+
+    return true;
+  }
 
   function onChange(e) {
     e.persist();
@@ -19,47 +29,58 @@ function ReservationForm({ setReservations }) {
 
   function submitReservation(e) {
     e.preventDefault();
-    postReservation(form).then(data =>  setReservations(prev => [...prev, data]) )
-   
-    setForm({ name: '', date: '', time: '', number: '' });
+
+    if (validate(form)) {
+      postReservation(form).then(data =>
+        setReservations(prev => [...prev, data]),
+      );
+      setForm({ name: '', date: '', time: '', number: '' });
+    }
   }
 
   return (
-    <form>
-      <input
-        id='name'
-        type='text'
-        value={form.name}
-        placeholder='Name'
-        onChange={e => onChange(e)}
-      />
-      <input
-        id='date'
-        type='text'
-        value={form.date}
-        placeholder='Date(mm/dd)'
-        onChange={e => onChange(e)}
-      />
-      <input
-        id='time'
-        type='text'
-        value={form.time}
-        placeholder='Time'
-        onChange={e => onChange(e)}
-      />
-      <input
-        id='number'
-        type='text'
-        value={form.number}
-        placeholder='Number of guests'
-        onChange={e => onChange(e)}
-      />
-      <input
-        type='submit'
-        value='Make Reservation'
-        onClick={e => submitReservation(e)}
-      />
-    </form>
+    <div className='reservationForm'>
+      <p className='formNotice'>{formError ? formError : 'Make a reservation!'}</p>
+      <form>
+        <input
+          id='name'
+          type='text'
+          value={form.name}
+          placeholder='Name'
+          onChange={e => onChange(e)}
+          required
+        />
+        <input
+          id='date'
+          type='text'
+          value={form.date}
+          placeholder='Date(mm/dd)'
+          onChange={e => onChange(e)}
+          required
+        />
+        <input
+          id='time'
+          type='text'
+          value={form.time}
+          placeholder='Time'
+          onChange={e => onChange(e)}
+          required
+        />
+        <input
+          id='number'
+          type='text'
+          value={form.number}
+          placeholder='Number of guests'
+          onChange={e => onChange(e)}
+          required
+        />
+        <input
+          type='submit'
+          value='Make Reservation'
+          onClick={e => submitReservation(e)}
+        />
+      </form>
+    </div>
   );
 }
 
