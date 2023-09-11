04282c801ee7437cec0d448c117979d9ac841d12
Renee Pinna <renee.pinna@gmail.com>
2023-09-11 09:40:47 -0600

    Add ReservationCard component

---
+++ src/ReservationCard/ReservationCard.js
@@ -0,0 +1,16 @@
+import 'ReservationCard.css';
+
+function ReservationCard({ reservation }) {
+  const { id, name, date, time, number } = reservation;
+
+  return (
+    <article className='reservationCard'>
+      <h3 className='card-name'>{name}</h3>
+      <p className='card-text'>{date}</p>
+      <p className='card-text'>{time}</p>
+      <p className='card-text'>{number}</p>
+    </article>
+  );
+}
+
+export default ReservationCard;
